e3b5fe6aba71558e30b5c134cbc29af71f680281
JoseABravoD <bravo.jandres05@gmail.com>
2025-10-22 11:23:29 -0400

    Update mini_ejercicio1.py
    Ahora si, cambios en el mini ejercicio

--- Module 1/mini_ejercicio1.py
+++ Module 1/mini_ejercicio1.py
@@ -15,7 +15,6 @@ Este ejercicio combina:
 """
 
 import pygame
-import random  # Para elegir colores aleatorios
 
 pygame.init()
 
@@ -25,10 +24,17 @@ ventana = pygame.display.set_mode((800, 600))
 #Titulo de la ventana
 pygame.display.set_caption("Mini ejercicio: fondo aleatorio")
 
+cont = 0
 corriendo = True
 while corriendo:
-    Red = random.randint(0, 10)
-    ventana.fill(Red , 50, 50)
+    
+    if cont < 256:
+        ventana.fill((50, 50, cont))
+        cont += 0.2
+    else:
+        cont = 0
+    
+    pygame.display.flip()
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             corriendo = False
